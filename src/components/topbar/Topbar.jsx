2852211d5630d06b40faf8d5963ccfc3f2dbb930
NahrulK <khayattullah73@gmail.com>
2022-04-02 12:05:45 +0800

    Topbar Component

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,35 @@
+import { Language, NotificationsNone, Settings } from "@material-ui/icons";
+import React from "react";
+import "./topbar.css";
+
+export default function Topbar() {
+  return (
+    <div>
+      <div className="topbar">
+        <div className="topbarWrapper">
+          <div className="topLeft">
+            <span className="logo">getamaadmin</span>
+          </div>
+          <div className="topRight">
+            <div className="topbarIconContainer">
+              <NotificationsNone />
+              <span className="topIconBadge">2</span>
+            </div>
+            <div className="topbarIconContainer">
+              <Language />
+              <span className="topIconBadge">2</span>
+            </div>
+            <div className="topbarIconContainer">
+              <Settings />
+            </div>
+            <img
+              src="https://drive.google.com/uc?id=1SHBPvrYUZG_3qxsdSI__P-pPmhFF4Zhm"
+              alt=""
+              className="topAvatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
