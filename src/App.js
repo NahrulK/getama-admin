63bc732bb7ad7ab16c6c7ae40e5249d608d4abe6
NahrulK <khayattullah73@gmail.com>
2022-04-02 13:07:38 +0800

    Sidebar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Topbar></Topbar>
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 }
