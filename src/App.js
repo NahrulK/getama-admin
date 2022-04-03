1d3a3ebbd6dc534fca1775f0d3b989ca41ce7336
NahrulK <khayattullah73@gmail.com>
2022-04-03 12:45:04 +0800

    User List Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
-      <Topbar></Topbar>
+    <Router>
+      <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
